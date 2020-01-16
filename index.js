d9914bdd5e777dc8fdeb7829bea94699b4e25175
Leah Migacz <lmigacz@msudenver.edu>
2020-01-15 20:49:49 -0700

    added variables for require

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const fs = require("fs");
+const axios = require('axios');
+const inquirer = require('inquirer');
+const { generateHTML } = require("./generateHTML.js")
+
 const questions = [
     {
         type: "input",
