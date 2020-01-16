2eb5b1799694d4041d125cef2531e33e78748b89
Leah Migacz <lmigacz@msudenver.edu>
2020-01-15 20:08:01 -0700

    added inquirer questions

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const questions = [
-  
+    {
+        type: "input",
+        name: "username",
+        message: "Please enter your GitHub username"
+    },
+    {
+        type: "list",
+        name: "color",
+        message: "What is your favorite color?",
+        choices: ['green', 'blue', 'pink', 'red'],
+    }
+
 ];
 
 function writeToFile(fileName, data) {
